22738bd20130666530fd565559b7476f13484865
Chris <chris@intriguingdevelopments.com>
2025-01-05 21:32:11 +0000

    'public' access where needed

--- Sources/PDFRenderer/PageSize.swift
+++ Sources/PDFRenderer/PageSize.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-enum PageSize {
-    enum A4 {
-        static var landscape: CGSize { CGSize(width: 297, height: 210) }
-        static var portrait: CGSize { CGSize(width: 210, height: 297) }
+public enum PageSize {
+    public enum A4 {
+        public static var landscape: CGSize { CGSize(width: 297, height: 210) }
+        public static var portrait: CGSize { CGSize(width: 210, height: 297) }
     }
 }
 
-extension CGSize {
+public extension CGSize {
     func postscriptSize() -> CGSize {
         let mmFactor = 72.0 / 25.4
 
